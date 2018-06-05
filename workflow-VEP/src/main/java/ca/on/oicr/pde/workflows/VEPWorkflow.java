f1618ff3c412d59437fa3ecefd829801293b2379
prisnir <Prisni.Rath@oicr.on.ca>
2018-06-05 12:07:53 -0400

    workflow new edit with module load

--- workflow-VEP/src/main/java/ca/on/oicr/pde/workflows/VEPWorkflow.java
+++ workflow-VEP/src/main/java/ca/on/oicr/pde/workflows/VEPWorkflow.java
@@ -57,10 +57,10 @@ public class VEPWorkflow extends OicrWorkflow {
     //params
     private String hgBuild;
     private String species;
-    private String annotInfo = ",gnomAD,vcf,exact,0,AF_POPMAX,AF_AFR,AF_AMR,AF_ASJ,AF_EAS,AF_FIN,AF_NFE,AF_OTH,AF_SAS";
+//    private String annotInfo = ",gnomAD,vcf,exact,0,AF_POPMAX,AF_AFR,AF_AMR,AF_ASJ,AF_EAS,AF_FIN,AF_NFE,AF_OTH,AF_SAS";
     private Integer hgvsShift = 1;
     private double vafFilter = 0.7;
-    private String retainInfo = "gnomAD_AF_POPMAX,gnomAD_AF_AFR,gnomAD_AF_AMR,gnomAD_AF_ASJ,gnomAD_AF_EAS,gnomAD_AF_FIN,gnomAD_AF_NFE,gnomAD_AF_OTH,gnomAD_AF_SAS";
+//    private String retainInfo = "gnomAD_AF_POPMAX,gnomAD_AF_AFR,gnomAD_AF_AMR,gnomAD_AF_ASJ,gnomAD_AF_EAS,gnomAD_AF_FIN,gnomAD_AF_NFE,gnomAD_AF_OTH,gnomAD_AF_SAS";
     
 
 
@@ -95,33 +95,18 @@ public class VEPWorkflow extends OicrWorkflow {
             outputFilenamePrefix = getProperty("external_identifier");
             normalSamplePrefix = getOptionalProperty("normal_sample_external_identifier", "NA");
 
-            //tools
-            vcf2maf = getProperty("vcf2maf");
-            vep = getProperty("VEP");
-            samtools = getProperty("samtools");
 
             // ref fasta
             refFasta = getProperty("ref_fasta");
             exacVCF = getProperty("ExAC_vcf");
-            gnomadVCF = getProperty("GNOMAD_vcf");
             
             
             // VEP
-            vepPath = getProperty("VEP_PATH");
-            vepData = getProperty("VEP_DATA");
             hgvsShift = Integer.parseInt(getOptionalProperty("hgvs_shift_flag", "1"));
             vafFilter = Double.parseDouble(getOptionalProperty("vaf_filter", "0.7"));
             species = getOptionalProperty("species", "homo_sapiens");
             hgBuild = getOptionalProperty("hg_version", "GRCh37");
             
-            // Environment vars
-            perl5lib=getProperty("perl5lib");
-            envVars = "export VEP_PATH="+this.vepPath+";";
-            envVars = envVars + "export VEP_DATA="+vepData+";";
-            envVars = envVars + "export PERL5LIB=$VEP_DATA:"+this.perl5lib+"; ";
-            envVars = envVars + "export PATH=$VEP_PATH/htslib:$PATH; ";
-            envVars = envVars + "export PATH="+this.samtools+":$PATH; ";
-            envVars = envVars + "unset PYTHONPATH ; " + "module load python-gsi/2.7.12 ;";
 
             manualOutput = Boolean.parseBoolean(getProperty("manual_output"));
             queue = getOptionalProperty("queue", "");
@@ -168,7 +153,6 @@ public class VEPWorkflow extends OicrWorkflow {
         String inVCF = getFiles().get("inVCF").getProvisionedPath();
         String tmpVCF = this.tmpDir + this.outputFilenamePrefix + ".temp.vcf";
         
-        String annoGNOMADvcf = this.tmpDir + this.outputFilenamePrefix + "_gnomad" + ".vcf";
         String mafFile = this.outDir + this.outputFilenamePrefix + ".maf.txt";
         if (normalSamplePrefix == null || normalSamplePrefix == "NA"){
             this.normalSamplePrefix = this.outputFilenamePrefix;
@@ -185,12 +169,10 @@ public class VEPWorkflow extends OicrWorkflow {
          preprocessVCF.setMaxMemory(Integer.toString(this.VEPMem * 1024));
          preprocessVCF.setQueue(getOptionalProperty("queue", ""));
          parentJob = preprocessVCF;
-    
-        Job annotateGNOMAD = annotateGnomad(tmpVCF, annoGNOMADvcf);
-        annotateGNOMAD.addParent(parentJob);
-        parentJob = annotateGNOMAD;
+
+        // Job pending to do: annotate OICR frequency
         
-        Job vcf2MAF = runVcf2Maf(annoGNOMADvcf, mafFile);
+        Job vcf2MAF = runVcf2Maf(tmpVCF, mafFile);
         vcf2MAF.addParent(parentJob);
         parentJob = vcf2MAF;
             
@@ -201,58 +183,24 @@ public class VEPWorkflow extends OicrWorkflow {
         vcf2MAF.addFile(outMaf);
         
     }
-      
-    private Job annotateGnomad(String inVCF, String gnomadVCF){
-        Job annoGnomad = getWorkflow().createBashJob("annotate_gnomad");
-        Command cmd = annoGnomad.getCommand();
-        cmd.addArgument("module load perl/"+perlVersion + ";");
-        cmd.addArgument(this.envVars);
-        cmd.addArgument("perl "+this.vep);
-        cmd.addArgument("--species "+ this.species);
-        cmd.addArgument("--assembly "+ this.hgBuild);
-        cmd.addArgument("--offline");
-        cmd.addArgument("--no_progress");
-        cmd.addArgument("--everything");
-        cmd.addArgument("--shift_hgvs "+ Integer.toString(this.hgvsShift));
-        cmd.addArgument("--check_existing");
-        cmd.addArgument("--check_alleles");
-        cmd.addArgument("--total_length");
-        cmd.addArgument("--allele_number");
-        cmd.addArgument("--no_escape");
-        cmd.addArgument("--xref_refseq");
-        cmd.addArgument("buffer_size "+"200");
-        cmd.addArgument("--dir "+this.vepData);
-        cmd.addArgument("--fasta "+this.refFasta);
-        cmd.addArgument("--input_file "+ inVCF);
-        cmd.addArgument("--force_overwrite");
-        cmd.addArgument("--custom "+ this.gnomadVCF + this.annotInfo);
-        cmd.addArgument("--vcf");
-        cmd.addArgument("output_file "+ gnomadVCF);
-        annoGnomad.setMaxMemory(Integer.toString(this.VEPMem * 1024));
-        annoGnomad.setQueue(getOptionalProperty("queue", ""));
-        return annoGnomad;
-    }
-    
-    private Job runVcf2Maf(String annoGNOMADvcf, String outputMAF){
+          
+    private Job runVcf2Maf(String inVCF, String outputMAF){
         Job runVCF2MAF = getWorkflow().createBashJob("vcf2maf");
         Command cmd = runVCF2MAF.getCommand();
-        cmd.addArgument("module load perl/"+perlVersion+ ";");
-        cmd.addArgument(this.envVars);
-        cmd.addArgument("perl "+this.vcf2maf);
+        cmd.addArgument("module load vcf2maf;");
+        cmd.addArgument("vcf2maf.pl");
         cmd.addArgument("--species "+ this.species);
-        cmd.addArgument("--ncbi-build" + this.hgBuild);
-        cmd.addArgument("--input-vcf " + annoGNOMADvcf);
+        cmd.addArgument("--ncbi-build " + this.hgBuild);
+        cmd.addArgument("--input-vcf " + inVCF);
         cmd.addArgument("--output-maf "+ outputMAF);
         cmd.addArgument("--tumor-id " + this.outputFilenamePrefix);
         cmd.addArgument("--normal-id " + this.normalSamplePrefix);
         cmd.addArgument("--vcf-tumor-id " + this.outputFilenamePrefix);
         cmd.addArgument("--vcf-normal-id " + this.normalSamplePrefix);
-        cmd.addArgument("--vep-path "+ this.vepPath);
-        cmd.addArgument("--vep-data "+ this.vepData);
         cmd.addArgument("--ref-fasta "+this.refFasta);
         cmd.addArgument("--filter-vcf "+this.exacVCF);
         cmd.addArgument("--max-filter-ac 10");
-        cmd.addArgument("--retain-info " + this.retainInfo);
+//        cmd.addArgument("--retain-info " + this.retainInfo);
         cmd.addArgument("--min-hom-vaf "+ Double.toString(this.vafFilter));
         runVCF2MAF.setMaxMemory(Integer.toString(this.VEPMem * 1024));
         runVCF2MAF.setQueue(getOptionalProperty("queue", ""));
